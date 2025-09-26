b680705f47004ec0d45ad89795c1b790ff4f8f83
Ahmed Mohammed Jayanti Mochi <amochi670@rku.ac.in>
2025-09-27 03:04:06 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,5 @@
 //your JS code here. If required.
 document.addEventListener('DOMContentLoaded', () => {
-	document.body.innerHTML = '';
-  let p = document.createElement("p");
-  p.innerText = "DOM load success";
-  document.body.appendChild(p);
+	document.body.innerHTML = ''; 
+	document.body.appendChild(document.createTextNode("DOM load success"));
 })
